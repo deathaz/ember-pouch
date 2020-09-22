67b1e040c443e660ca25028ceb4217072414ec44
Dev Chong <deathaz.chong@gmail.com>
2020-09-23 01:12:45 +0800

    upgrade to v3.21.2 by ecu

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,133 +6,6 @@ module.exports = async function() {
   return {
     scenarios: [
       {
-        name: 'ember-2.4-stack',
-        npm: {
-          devDependencies: {
-            'ember-data': '2.4.3',
-            'ember-inflector': '^1.9.4',
-            'ember-source': null,
-            'ember-cli-shims': null
-          }
-        },
-        bower: {
-          dependencies: {
-            'ember': '2.4.6',
-            'ember-cli-shims': '0.1.1'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-2.8',
-        bower: {
-          dependencies: {
-            'ember': 'components/ember#lts-2-8',
-            'ember-cli-shims': '0.1.1'
-          },
-          resolutions: {
-            'ember': 'lts-2-8'
-          }
-        },
-        npm: {
-          devDependencies: {
-            'ember-data': '2.8.1',
-            'ember-inflector': '^1.9.4',
-            'ember-source': null,
-            'ember-cli-shims': null
-          }
-        }
-      },
-      {
-        name: 'ember-lts-2.12',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-data': '2.12.2',
-            'ember-inflector': '^1.9.4',
-            'ember-source': '2.12.2',
-            'ember-cli-shims': '^1.1.0'
-          }
-        }
-      },
-      {
-        name: 'ember-2.14-stack',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-data': '~2.14.0',
-            'ember-source': '~2.14.0',
-          }
-        },
-      },
-      {
-        name: 'ember-lts-2.16',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-data': '~2.16.0',
-            'ember-source': '~2.16.0',
-          }
-        }
-      },
-      {
-        name: 'ember-lts-2.18',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-data': '~2.18.0',
-            'ember-source': '~2.18.0',
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.4.0',
-            'ember-data': '~3.4.0',
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.8.0',
-            'ember-data': '~3.8.0',
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0',
-            'ember-data': '~3.12.0',
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.16',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.16.0',
-            'ember-data': '~3.16.0',
-          }
-        }
-      },
-      {
         name: 'ember-release',
         npm: {
           devDependencies: {
@@ -159,16 +32,6 @@ module.exports = async function() {
           }
         }
       },
-      // The default `.travis.yml` runs this scenario via `npm test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
-      {
-        name: 'ember-default',
-        npm: {
-          devDependencies: {}
-        }
-      },
       {
         name: 'ember-default-with-jquery',
         env: {
@@ -178,7 +41,7 @@ module.exports = async function() {
         },
         npm: {
           devDependencies: {
-            '@ember/jquery': '^0.5.1'
+            '@ember/jquery': '^1.1.0'
           }
         }
       },
